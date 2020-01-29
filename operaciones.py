fd6790ccec5eebdb9ddc82df9e987dab27cc9c0d
Felix Nader <fronteranorte@yahoo.com.mx>
2020-01-29 12:48:15 -0500

    Nueva aproximacion

---
+++ operaciones.py
@@ -0,0 +1,28 @@
+
+#calcula cuntos billetes necesito
+def necesito(monto,d):
+    return monto/d
+#Calcula denominacion * billetes
+def monto_denom(valor,metalico):
+    return metalico*valor
+#Resta alguna cantidad y devuelve el saldo
+def nuevoMonto(monto,restado):
+    return monto-restado
+
+#monto=monto
+#monto a separar en denominaciones
+#for d in denom.den:
+#itera la denominacion
+    #Calcula si alcanza billetes de la denominacion
+    #if alcanza * d > 0:
+        #print('monto alcanza ' , alcanza , 'billetes de ' ,d)
+        #valor=alcanza*d
+        #print(monto, '-', valor)
+        #si el valor monetario de la denominacion rebasa el monto
+        #actual de la denominacion, imprime el valor actual del monto
+         #y el valor a deducir del mismo'''
+    #if  alcanza * d  >= d:
+        #monto=monto-(alcanza*d)
+        #print(monto)
+        #deduce el monto pendiente lo asigna como nuevo
+        #monto el imprime el nuevo monto a deducir'''
